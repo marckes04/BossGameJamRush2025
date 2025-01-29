54ddcd53e920070f3c1551e21cc150642e9636ea
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-01-29 00:05:56 -0500

    Enemy Preparation

---
+++ Assets/Scripts/Player/CharacterAnimationDelegate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationDelegate : MonoBehaviour
+{
+    public GameObject attackPoint;
+
+
+   void AttackPointOn()
+    {
+        attackPoint.SetActive(true);
+    }
+
+
+    void AttackPointOff()
+    {
+        attackPoint.SetActive(true);
+    }
+}
